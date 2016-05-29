e590913fcf0e270da47fdac7f584da6d6e894fb9
nleoutsa <nleoutsa@gmail.com>
2016-05-29 01:59:16 -0400

    Begin PickupParent.cs script for hand controllers
    master

--- Assets/Scripts/PickupParent.cs
+++ Assets/Scripts/PickupParent.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SteamVR_TrackedObject))]  
 public class PickupParent : MonoBehaviour {
+    SteamVR_TrackedObject tracked_obj;
 
-	// Use this for initialization
-	void Start () {
-	
+    // This function is always called before any Start functions 
+    // and also just after a prefab is instantiated. 
+    // (If a GameObject is inactive during start up Awake is not called until it is made active.)
+    void Awake () {
+        tracked_obj = GetComponent<SteamVR_TrackedObject>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // FixedUpdate is called once for every physics step.
+    // Use for regular updates to rigid bodies affected by physics
+    void FixedUpdate () {
+        // create variable to represent input device (hand controller)
+        Device device = SteamVR_Controller.Input((int)tracked_obj.index);
+    }
 }
