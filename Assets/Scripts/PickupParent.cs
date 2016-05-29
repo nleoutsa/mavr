e3c066fb4dca6343f9bae6d1af7934540727d5fd
nleoutsa <nleoutsa@gmail.com>
2016-05-29 18:36:14 -0400

    Return objects to initial position.
    master

--- Assets/Scripts/PickupParent.cs
+++ Assets/Scripts/PickupParent.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
@@ -7,6 +8,9 @@ public class PickupParent : MonoBehaviour
 {
     SteamVR_TrackedObject tracked_obj;
     SteamVR_Controller.Device device;
+
+
+    List<Rigidbody> tossedObjects = new List<Rigidbody>();
 
     // This function is always called before any Start functions 
     // and also just after a prefab is instantiated. 
@@ -20,30 +24,35 @@ public class PickupParent : MonoBehaviour
     // Use for regular updates to rigid bodies affected by physics
     void FixedUpdate()
     {
-        // create variable to represent input device (hand controller)
+        // Create variable to represent input device (hand controller)
         device = SteamVR_Controller.Input((int)tracked_obj.index);
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad) && tossedObjects.Count > 0)
+        {
+            resetTossedObjectPosition();
+        }
     }
 
     void OnTriggerStay(Collider col)
     {
         Debug.Log("Collied with " + col.name + " and called OnTriggerStay");
-
+        
         if (col.attachedRigidbody)
-        {
-            if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {   
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
             {
                 Debug.Log("You have collided with " + col.name + " while holding down touch on the trigger");
 
                 col.attachedRigidbody.isKinematic = true;
-                col.gameObject.transform.SetParent(this.gameObject.transform);
+                col.gameObject.transform.SetParent(gameObject.transform);
             };
 
-            if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
             {
                 Debug.Log("You have released the trigger while parenting " + col.name);
 
                 col.gameObject.transform.SetParent(null);
-                col.attachedRigidbody.isKinematic = false;
+                col.attachedRigidbody.isKinematic = col.attachedRigidbody.GetComponent<TossableObject>().isKinematic;
 
                 tossChildObject(col.attachedRigidbody);
             };
@@ -52,6 +61,7 @@ public class PickupParent : MonoBehaviour
 
     private void tossChildObject(Rigidbody rigidbody)
     {
+
         Transform origin = tracked_obj.origin ? tracked_obj.origin : tracked_obj.transform.parent;
 
         if (origin != null)
@@ -61,9 +71,21 @@ public class PickupParent : MonoBehaviour
         } else
         {
             rigidbody.velocity = device.velocity;
-            rigidbody.angularvelocity = device.angularvelocity;
+            rigidbody.angularVelocity = device.angularVelocity;
         }
 
-        throw new NotImplementedException();
+        // Add tossed object to list of tossed objects.
+        tossedObjects.Add(rigidbody);
+    }
+
+    private void resetTossedObjectPosition()
+    {
+        tossedObjects[0].transform.position = tossedObjects[0].GetComponent<TossableObject>().initialPosition;
+        tossedObjects[0].velocity = Vector3.zero;
+        tossedObjects[0].angularVelocity = Vector3.zero;
+
+        // Remove object tossed least recently from list of tossed objects.
+        // This way, we reset object positions in order of when they were tossed.
+        tossedObjects.RemoveAt(0);
     }
 }
---
+++ Assets/Scripts/TossableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TossableObject : MonoBehaviour {
+    public bool isKinematic;
+    public Vector3 initialPosition;
+
+	// Use this for initialization
+	void Awake () {
+        isKinematic = GetComponent<Rigidbody>().isKinematic;
+        initialPosition = gameObject.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
