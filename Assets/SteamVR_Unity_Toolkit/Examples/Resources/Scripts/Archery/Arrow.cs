504d4f3f7db4dfd59850390cd59503d39af0df7e
nleoutsa <nleoutsa@gmail.com>
2016-10-23 15:27:25 -0400

    Make bow work with VRTK update
    backup

---
+++ Assets/SteamVR_Unity_Toolkit/Examples/Resources/Scripts/Archery/Arrow.cs
@@ -0,0 +1,123 @@
+﻿namespace VRTK.Examples.Archery
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class Arrow : MonoBehaviour
+    {
+        public float maxArrowLife = 600f;
+
+        [HideInInspector] public bool inFlight = false;
+
+        private bool collided = false;
+        private Rigidbody rigidBody;
+        private GameObject arrowHolder;
+        private Vector3 originalPosition;
+        private Quaternion originalRotation;
+        private Vector3 originalScale;
+
+        public void SetArrowHolder(GameObject holder)
+        {
+            arrowHolder = holder;
+            arrowHolder.SetActive(false);
+        }
+
+        public void OnNock()
+        {
+            collided = false;
+            inFlight = false;
+        }
+
+        public void Fired()
+        {
+            StartCoroutine(EmitSmoke());
+            DestroyArrow(maxArrowLife);
+        }
+
+        public void ResetArrow()
+        {
+            collided = true;
+            inFlight = false;
+            RecreateNotch();
+            ResetTransform();
+        }
+
+        private void Start()
+        {
+            rigidBody = GetComponent<Rigidbody>();
+            SetOrigns();
+        }
+
+        private void SetOrigns()
+        {
+            originalPosition = transform.localPosition;
+            originalRotation = transform.localRotation;
+            originalScale = transform.localScale;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!collided)
+            {
+                transform.LookAt(transform.position + rigidBody.velocity);
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (inFlight)
+            {
+                Debug.Log("Collided with " + collision.gameObject.name);
+                StickAfterCollision(collision);
+            }
+        }
+
+        private void RecreateNotch()
+        {
+            //swap the arrow holder to be the parent again
+            arrowHolder.transform.parent = null;
+            arrowHolder.SetActive(true);
+
+            //make the arrow a child of the holder again
+            transform.parent = arrowHolder.transform;
+
+            //reset the state of the rigidbodies and colliders
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Collider>().enabled = false;
+            arrowHolder.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        private void ResetTransform()
+        {
+            arrowHolder.transform.position = transform.position;
+            arrowHolder.transform.rotation = transform.rotation;
+            transform.localPosition = originalPosition;
+            transform.localRotation = originalRotation;
+            transform.localScale = originalScale;
+        }
+
+        private void DestroyArrow(float time)
+        {
+            Destroy(arrowHolder, time);
+            Destroy(gameObject, time);
+        }
+
+        IEnumerator EmitSmoke()
+        {
+            yield return new WaitForSeconds(0.05f);
+            gameObject.GetComponent<ParticleSystem>().Play();
+        }
+
+        public void StickAfterCollision(Collision collision)
+        {
+            collided = true;
+            inFlight = false;
+
+            // child the arrow to the collided object
+            transform.parent = collision.transform;
+            //Destroy the arrow's rigidbody2D and collider2D
+            Destroy(gameObject.GetComponent<Rigidbody>());
+            Destroy(gameObject.GetComponent<Collider>());
+        }
+    }
+}
--- Assets/SteamVR_Unity_Toolkit/Examples/Resources/Scripts/Archery/ArrowSpawner.cs
+++ Assets/SteamVR_Unity_Toolkit/Examples/Resources/Scripts/Archery/ArrowSpawner.cs
@@ -1,59 +1,49 @@
-﻿using UnityEngine;
-using VRTK;
-
-public class ArrowSpawner : MonoBehaviour
+﻿namespace VRTK.Examples.Archery
 {
-    public GameObject arrowPrefab;
-    public float spawnDelay = 1f;
+    using UnityEngine;
 
-    private float spawnDelayTimer = 0f;
-    private BowAim bow;
-
-    private void Start()
+    public class ArrowSpawner : MonoBehaviour
     {
-        spawnDelayTimer = 0f;
-    }
+        public GameObject arrowPrefab;
+        public float spawnDelay = 1f;
 
-    private void OnTriggerStay(Collider collider)
-    {
-        VRTK_InteractGrab grabbingController = collider.gameObject.GetComponentInParent<VRTK_InteractGrab>();
-        if (!grabbingController)
+        private float spawnDelayTimer = 0f;
+        private BowAim bow;
+
+        private void Start()
         {
-            return;
+            spawnDelayTimer = 0f;
         }
 
-        bool canGrab = CanGrab(grabbingController);
-        bool noArrowNotched = NoArrowNotched(grabbingController.gameObject);
-        bool deltaTime = Time.time >= spawnDelayTimer;
-
-//        if (CanGrab(grabbingController) && NoArrowNotched(grabbingController.gameObject) && Time.time >= spawnDelayTimer)
-        if (canGrab && noArrowNotched && deltaTime)
+        private void OnTriggerStay(Collider collider)
         {
-            print("success");
-            GameObject newArrow = Instantiate(arrowPrefab);
-            newArrow.name = "ArrowClone";
-            grabbingController.gameObject.GetComponent<VRTK_InteractTouch>().ForceTouch(newArrow);
-            grabbingController.AttemptGrab();
-            spawnDelayTimer = Time.time + spawnDelay;
-        }
-    }
-
-    private bool CanGrab(VRTK_InteractGrab grabbingController)
-    {
-        return (grabbingController && grabbingController.GetGrabbedObject() == null && grabbingController.gameObject.GetComponent<VRTK_ControllerEvents>().grabPressed);
-    }
-
-    private bool NoArrowNotched(GameObject controller)
-    {
-        if (controller == controllers.left)
-        {
-            bow = controllers.right.GetComponentInChildren<BowAim>();
-        }
-        else if (controller == controllers.right)
-        {
-            bow = controllers.left.GetComponentInChildren<BowAim>();
+            VRTK_InteractGrab grabbingController = (collider.gameObject.GetComponent<VRTK_InteractGrab>() ? collider.gameObject.GetComponent<VRTK_InteractGrab>() : collider.gameObject.GetComponentInParent<VRTK_InteractGrab>());
+            if (CanGrab(grabbingController) && NoArrowNotched(grabbingController.gameObject) && Time.time >= spawnDelayTimer)
+            {
+                GameObject newArrow = Instantiate(arrowPrefab);
+                newArrow.name = "ArrowClone";
+                grabbingController.gameObject.GetComponent<VRTK_InteractTouch>().ForceTouch(newArrow);
+                grabbingController.AttemptGrab();
+                spawnDelayTimer = Time.time + spawnDelay;
+            }
         }
 
-        return (bow == null || !bow.HasArrow());
+        private bool CanGrab(VRTK_InteractGrab grabbingController)
+        {
+            return (grabbingController && grabbingController.GetGrabbedObject() == null && grabbingController.gameObject.GetComponent<VRTK_ControllerEvents>().grabPressed);
+        }
+
+        private bool NoArrowNotched(GameObject controller)
+        {
+            if (VRTK_SDK_Bridge.IsControllerLeftHand(controller))
+            {
+                bow = VRTK_DeviceFinder.GetControllerRightHand().GetComponentInChildren<BowAim>();
+            }
+            else if (VRTK_SDK_Bridge.IsControllerRightHand(controller))
+            {
+                bow = VRTK_DeviceFinder.GetControllerLeftHand().GetComponentInChildren<BowAim>();
+            }
+            return (bow == null || !bow.HasArrow());
+        }
     }
 }
