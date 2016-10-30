43f20dd77574917dbbab989dec792a74d1123864
nleoutsa <nleoutsa@gmail.com>
2016-10-30 16:41:28 -0400

    Player can put arrows back in quiver. Quiver quantity and max
    QuiverUpdates

--- Assets/SteamVR_Unity_Toolkit/Examples/Resources/Scripts/Archery/ArrowSpawner.cs
+++ Assets/SteamVR_Unity_Toolkit/Examples/Resources/Scripts/Archery/ArrowSpawner.cs
@@ -1,4 +1,8 @@
-﻿namespace VRTK.Examples.Archery
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace VRTK.Examples.Archery
 {
     using UnityEngine;
 
@@ -6,9 +10,13 @@
     {
         public GameObject arrowPrefab;
         public float spawnDelay = 1f;
+        public int arrowsLeft = 50;
+        public int quiverCapacity = 50;
 
         private float spawnDelayTimer = 0f;
         private BowAim bow;
+        private bool controllerInQuiver = false;
+        private List<VRTK_InteractGrab> controllersListening = new List<VRTK_InteractGrab>();
 
         private void Start()
         {
@@ -18,19 +26,76 @@
         private void OnTriggerStay(Collider collider)
         {
             VRTK_InteractGrab grabbingController = (collider.gameObject.GetComponent<VRTK_InteractGrab>() ? collider.gameObject.GetComponent<VRTK_InteractGrab>() : collider.gameObject.GetComponentInParent<VRTK_InteractGrab>());
-            if (CanGrab(grabbingController) && NoArrowNotched(grabbingController.gameObject) && Time.time >= spawnDelayTimer)
+
+            if (arrowsLeft > 0 && CanGrab(grabbingController) && NoArrowNotched(grabbingController.gameObject) && Time.time >= spawnDelayTimer)
             {
-                GameObject newArrow = Instantiate(arrowPrefab);
-                newArrow.name = "ArrowClone";
-                grabbingController.gameObject.GetComponent<VRTK_InteractTouch>().ForceTouch(newArrow);
-                grabbingController.AttemptGrab();
-                spawnDelayTimer = Time.time + spawnDelay;
+                PickupArrow(grabbingController);
+            }
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            VRTK_InteractGrab grabbingController = (collider.gameObject.GetComponent<VRTK_InteractGrab>() ? collider.gameObject.GetComponent<VRTK_InteractGrab>() : collider.gameObject.GetComponentInParent<VRTK_InteractGrab>());
+
+            if (grabbingController)
+            {
+                controllerInQuiver = true;
+
+                if (!controllersListening.Contains(grabbingController))
+                {
+                    grabbingController.ControllerUngrabInteractableObject += new ObjectInteractEventHandler(DropArrow);
+                    controllersListening.Add(grabbingController);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider collider)
+        {
+            VRTK_InteractGrab grabbingController = (collider.gameObject.GetComponent<VRTK_InteractGrab>() ? collider.gameObject.GetComponent<VRTK_InteractGrab>() : collider.gameObject.GetComponentInParent<VRTK_InteractGrab>());
+
+            if (grabbingController)
+            {
+                controllerInQuiver = false;
             }
         }
 
         private bool CanGrab(VRTK_InteractGrab grabbingController)
         {
             return (grabbingController && grabbingController.GetGrabbedObject() == null && grabbingController.gameObject.GetComponent<VRTK_ControllerEvents>().grabPressed);
+        }
+
+        private void RemoveArrows(int num)
+        {
+            arrowsLeft -= num;
+        }
+
+        private void AddArrows(int num)
+        {
+            arrowsLeft += num;
+        }
+
+        private void PickupArrow(VRTK_InteractGrab grabbingController)
+        {
+            GameObject newArrow = Instantiate(arrowPrefab);
+            newArrow.name = "ArrowClone";
+            grabbingController.gameObject.GetComponent<VRTK_InteractTouch>().ForceTouch(newArrow);
+            grabbingController.AttemptGrab();
+            spawnDelayTimer = Time.time + spawnDelay;
+            RemoveArrows(1);
+        }
+
+        private void DropArrow(object sender, ObjectInteractEventArgs e)
+        {
+            if (controllerInQuiver && arrowsLeft < quiverCapacity)
+            {
+                var grabbedArrow = e.target;
+
+                if (grabbedArrow.GetComponent<ArrowNotch>() != null)
+                {
+                    Destroy(grabbedArrow);
+                    AddArrows(1);
+                }
+            }
         }
 
         private bool NoArrowNotched(GameObject controller)
